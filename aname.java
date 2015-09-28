0940a1322c91193cba129f83d2eee6fa97eb3fca
lnberry <lnberry@compute-linux01.cs.ucalgary.ca>
2015-09-27 21:14:14 -0600

    further merged the if statements in aname.java, replaced 'bool' and 'iean' with a single boolean named 'inputCorrect'

--- aname.java
+++ aname.java
@@ -1,52 +1,28 @@
 public class aname
 {
-	public static boolean bool;
-	public static boolean iean;
+	public static boolean inputCorrect=false;
 	public static void main(String[] args)
 	{
 			assign1 driver = new assign1();
 			int size = Integer.parseInt(args[1]);
 			String alg = args[2];
 			String type = args[0];
-			inputalg(alg);
-			intype(type);
+			inputalg(alg, type);
 
-			if (bool = false)
+			if (inputCorrect = false)
 			{
-				throw new IllegalArgumentException("Error - that is not a valid sorting method");
-			}
-			if(iean = false)
-			{
-				throw new IllegalArgumentException("Error - that is not a valid algorithm name");
-			}
-			if (size <=0)
-			{
-				throw new NumberFormatException("Error- the size of the array must be bigger than zero");
+				throw new IllegalArgumentException("Error - input was incorrect");
 			}
 			driver.name(alg, size, type);
 			
 			
 	}
-	public static boolean	inputalg(String x)
+	public static boolean	inputalg(String x, String y)
 	{
-		if (x.equalsIgnoreCase("asscending")||x.equalsIgnoreCase("decending")|x.equalsIgnoreCase("random"))
+		if ((x.equalsIgnoreCase("asscending")||x.equalsIgnoreCase("decending")|x.equalsIgnoreCase("random"))&&(y.equalsIgnoreCase("bubblesort")||y.equalsIgnoreCase("insertionsort")||y.equalsIgnoreCase("mergesort")))
 		{
-			bool = true;
+			inputCorrect = true;
 		}
-		else
-			bool = false;
-		return bool;
-	}
-
-
-	public static boolean intype( String y)
-	{
-		if (y.equalsIgnoreCase("bubblesort")||y.equalsIgnoreCase("insertionsort")||y.equalsIgnoreCase("mergesort"))
-		{
-			iean = true;
-		}
-		else
-			iean = false;
-		return iean;
+		return inputCorrect;
 	}
 }
