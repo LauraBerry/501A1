1c1299fac23d9971d265aa7ae198fb0608546ea2
lnberry <lnberry@zone08-wb.cs.ucalgary.ca>
2015-09-29 11:23:24 -0600

    made output less confusing

--- assign1.java
+++ assign1.java
@@ -160,12 +160,16 @@ public class assign1
 	
 	public static void output (long time)
 	{
+		System.out.print("sorted list: ");
 		for (int i = 0; i < a.length; i++)
 		{
-			System.out.println(a[i]);
+			System.out.print(a[i]);
+			System.out.print(", ");
 		}
 		System.out.println("");
+		System.out.print("time: ");
 		System.out.println(time);
+		System.out.print("count: ");
 		System.out.println(cnt);
 	}
 	
