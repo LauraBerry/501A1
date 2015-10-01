8a697901b3fe68c9bea17ae5c1989782cce439d3
lnberry <lnberry@zone07-eb.cs.ucalgary.ca>
2015-10-01 11:06:10 -0600

    added new tests

--- AllTests.java
+++ AllTests.java
@@ -6,7 +6,7 @@ public class AllTests
 	@Test
 	public void testInuptChecks()
 	{
-		assertEquals(true, aname.inputalg ("asscending"));
+		assertEquals(true, aname.inputalg ("ascending"));
 		assertEquals(true, aname.inputalg ("random"));
 		assertEquals(true, aname.inputalg ("decending"));
 		
@@ -25,7 +25,7 @@ public class AllTests
 	
 
 	@Test
-	public void testSortingAlgs()
+	public void testSortingAlgsDecending()
 	{
 		assertEquals([1,2,3], Sorter.bubblesort([3,2,1]));
 		assertEquals([1,2,3], Sorter.insertionsort([3,2,1]));
@@ -33,9 +33,33 @@ public class AllTests
 	}
 
 	@Test
+	public void testSortingAlgsAscending()
+	{
+		assertEquals([1,2,3], Sorter.bubblesort([1,2,3]));
+		assertEquals([1,2,3], Sorter.insertionsort([1,2,3]));
+		assertEquals([1,2,3], Sorter.mergesort(0,1,[1,2,3]));
+	}
+	
+	@Test
+	public void testSortingAlgsNoOrder()
+	{
+		assertEquals([1,2,3], Sorter.bubblesort([3,1,2]));
+		assertEquals([1,2,3], Sorter.insertionsort([3,1,2]));
+		assertEquals([1,2,3], Sorter.mergesort(0,1,[3,1,2]));
+	}
+
+	@Test
+	public void testDuplicates()
+	{
+		assertEquals([1,1,3], Sorter.bubblesort([3,1,1]));
+		assertEquals([1,1,3], Sorter.insertionsort([3,1,1]));
+		assertEquals([1,1,3], Sorter.mergesort(0,1,[3,1,1]));
+	}
+
+	@Test
 	public void testArrayBuilder()
 	{
-		assertEquals([1,2,3], assign1.arr(2,"asscending", []));
+		assertEquals([1,2,3], assign1.arr(2,"ascending", []));
 		assertEquals([3,2,1], assign1.arr(2,"decending", []));
 	}
 
