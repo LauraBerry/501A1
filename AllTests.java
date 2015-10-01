bb9307f4f61f7815e38f6a3c185fb0bedf813ad4
lnberry <lnberry@zone07-wb.cs.ucalgary.ca>
2015-10-01 14:18:15 -0600

    updated tests

--- AllTests.java
+++ AllTests.java
@@ -1,66 +1,72 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
+
 
 public class AllTests
 {
 	@Test
 	public void testInuptChecks()
 	{
-		assertEquals(true, aname.inputalg ("ascending"));
-		assertEquals(true, aname.inputalg ("random"));
-		assertEquals(true, aname.inputalg ("decending"));
+		assertEquals(true, aname.inputalg ("ascending", "bubblesort"));
+		assertEquals(true, aname.inputalg ("random", "bubblesort"));
+		assertEquals(true, aname.inputalg ("decending","bubblesort"));
 		
-		assertEquals(true, aname.intype ("bubblesort"));
-		assertEquals(true, aname.inputalg ("insertionsort"));
-		assertEquals(true, aname.inputalg ("mergesort"));
+		assertEquals(true, aname.inputalg ("ascending", "insertionsort"));
+		assertEquals(true, aname.inputalg ("random", "insertionsort"));
+		assertEquals(true, aname.inputalg ("decending","insertionsort"));
+
+		assertEquals(true, aname.inputalg ("ascending", "mergesort"));
+		assertEquals(true, aname.inputalg ("random", "mergesort"));
+		assertEquals(true, aname.inputalg ("decending","mergesort"));
 	}
 	
 	@Test
 	public void testFindSort()
 	{
-		assertEqualls([1,2,3], Sorter.findSort([3,2,1],"bubblesort",2));
-		assertEqualls([1,2,3], Sorter.findSort([3,2,1],"insertionsort",2));
-		assertEqualls([1,2,3], Sorter.findSort([3,2,1],"mergesort",2));
+		assertArrayEquals(new int[] {1,2,3}, Sorter.findSort(new int[] {3,2,1},"bubblesort",2));
+		assertArrayEquals(new int[] {1,2,3}, Sorter.findSort(new int[] {3,2,1},"insertionsort",2));
+		assertArrayEquals(new int[] {1,2,3}, Sorter.findSort(new int[] {3,2,1},"mergesort",2));
 	}
 	
 
 	@Test
 	public void testSortingAlgsDecending()
 	{
-		assertEquals([1,2,3], Sorter.bubblesort([3,2,1]));
-		assertEquals([1,2,3], Sorter.insertionsort([3,2,1]));
-		assertEquals([1,2,3], Sorter.mergesort(0,1,[3,2,1]));
+		assertArrayEquals(new int[] {1,2,3}, Sorter.bubblesort(new int[] {3,2,1}));
+		assertArrayEquals(new int[] {1,2,3}, Sorter.insertionsort(new int[] {3,2,1}));
+		assertArrayEquals(new int[] {1,2,3}, Sorter.mergesort(0,1,new int[] {3,2,1}));
 	}
 
 	@Test
 	public void testSortingAlgsAscending()
 	{
-		assertEquals([1,2,3], Sorter.bubblesort([1,2,3]));
-		assertEquals([1,2,3], Sorter.insertionsort([1,2,3]));
-		assertEquals([1,2,3], Sorter.mergesort(0,1,[1,2,3]));
+		assertArrayEquals(new int[] {1,2,3}, Sorter.bubblesort(new int[] {1,2,3}));
+		assertArrayEquals(new int[] {1,2,3}, Sorter.insertionsort(new int[] {1,2,3}));
+		assertArrayEquals(new int[] {1,2,3}, Sorter.mergesort(0,1,new int[] {1,2,3}));
 	}
 	
 	@Test
 	public void testSortingAlgsNoOrder()
 	{
-		assertEquals([1,2,3], Sorter.bubblesort([3,1,2]));
-		assertEquals([1,2,3], Sorter.insertionsort([3,1,2]));
-		assertEquals([1,2,3], Sorter.mergesort(0,1,[3,1,2]));
+		assertArrayEquals(new int[] {1,2,3}, Sorter.bubblesort(new int[] {3,1,2}));
+		assertArrayEquals(new int[] {1,2,3}, Sorter.insertionsort(new int[] {3,1,2}));
+		assertArrayEquals(new int[] {1,2,3}, Sorter.mergesort(0,1,new int[] {3,1,2}));
 	}
 
 	@Test
 	public void testDuplicates()
 	{
-		assertEquals([1,1,3], Sorter.bubblesort([3,1,1]));
-		assertEquals([1,1,3], Sorter.insertionsort([3,1,1]));
-		assertEquals([1,1,3], Sorter.mergesort(0,1,[3,1,1]));
+		assertArrayEquals(new int[] {1,1,3}, Sorter.bubblesort(new int[] {3,1,1}));
+		assertArrayEquals(new int[] {1,1,3}, Sorter.insertionsort(new int[] {3,1,1}));
+		assertArrayEquals(new int[] {1,1,3}, Sorter.mergesort(0,1,new int[] {3,1,1}));
 	}
 
 	@Test
 	public void testArrayBuilder()
 	{
-		assertEquals([1,2,3], assign1.arr(2,"ascending", []));
-		assertEquals([3,2,1], assign1.arr(2,"decending", []));
+		assertArrayEquals(new int[] {1,2,3}, assign1.arr(2,"ascending", new int[] {}));
+		assertArrayEquals(new int[] {3,2,1}, assign1.arr(2,"decending", new int[] {}));
 	}
 
 
